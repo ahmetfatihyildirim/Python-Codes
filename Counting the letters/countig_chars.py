05a996803becf59566493f4526b1a071bedbac9d
A.Fatih Y <106478007+afy001@users.noreply.github.com>
2022-07-28 22:53:49 +0300

    Delete Counting the letters directory

--- Counting the letters/countig_chars.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Please write your sentence to Count the number of each letter in a sentence: ")
-output = {}
-setof = set(sentence)
-
-for i in setof:
-    output[i] = sentence.count(i)
-output
-
