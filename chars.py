ff374668c31e661a2aa0a61f7cf2c3bd2d974fcc
A.Fatih Y <106478007+afy001@users.noreply.github.com>
2022-07-28 22:49:24 +0300

    Create chars.py

---
+++ chars.py
@@ -0,0 +1,7 @@
+sentence = input("Please write your sentence to Count the number of each letter in a sentence: ")
+output = {}
+setof = set(sentence)
+
+for i in setof:
+    output[i] = sentence.count(i)
+output
